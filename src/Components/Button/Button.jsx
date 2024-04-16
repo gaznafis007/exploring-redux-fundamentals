c20adb24a7984af30c5e6d880f1d83e13d4c47f5
gazinafis007 <gazinafisrafi.gnr@gmail.com>
2024-04-16 20:38:16 +0600

    eslint correction

--- src/Components/Button/Button.jsx
+++ src/Components/Button/Button.jsx
@@ -1,5 +1,6 @@
 
 
+// eslint-disable-next-line react/prop-types
 const Button = ({children, btnColor, onClickHandler}) => {
   return (
     <span onClick={onClickHandler} className={`text-white px-4 py-2 ${btnColor} mr-2 capitalize rounded-md cursor-pointer`}>{children}</span>
